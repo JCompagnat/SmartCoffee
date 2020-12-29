11d95fb761e125cb99d452a93358dad6913c7d2f
JCompagnat <jessen.compagnat@hotmail.com>
2020-12-28 21:04:06 -0500

    Flask programming in progress

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 
+# initialize application
+from flask import Flask
+app = Flask(__name__)
+
+# get
+@app.route('/')  # the default is GET only
+def blog():
+    # ...
+
+#post
+@app.route('/new', methods=['POST'])
+def new():
+    # ...
+
+
+
+# run application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
