25a3d869aad4b58ec7580bd5e669a400a3387b3b
JCompagnat <jessen.compagnat@hotmail.com>
2021-01-28 18:58:41 -0500

    html work in progress

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/ManualOperations')
+@app.route('/manualoperations')
 def ManualOperations():
 	return render_template('manualoperations.html')
 
