8f80a6d8431d184ed8c4d7fca6036669a387df4f
JCompagnat <jessen.compagnat@hotmail.com>
2021-03-21 09:27:32 -0400

    Working on PID temp control

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ import time, math
 import RPi.GPIO as GPIO
 import max31865
 import random, json
+from simple_pid import PID
 app = Flask(__name__)
 
 
@@ -28,10 +29,14 @@ def plex():
 def _get_temp():
 
 	#tempC=random.randint(30, 110)
-	max = max31865.max31865()
 	tempC = max.readTemp()
+
+	control = pid(tempC)
+    pwm.ChangeDutyCycle(control)
+
+    p, i, d = pid.components
 	#tempC = max.readTemp()
-	return jsonify(temp=tempC)
+	return jsonify(temp=tempC, commandP=P, commandI=I, commandD=D)
 
 
 @app.route('/_set_temp')
@@ -85,4 +90,17 @@ def _set_time():
 
 # run application
 if __name__ == '__main__':
+
+	max = max31865.max31865()
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(23, GPIO.OUT)
+
+	pwm = GPIO.PWM(23,60)
+	pwm.start(10)
+	pwm.ChangeDutyCycle(30)
+
+	pid = PID(20, 0, 200, setpoint=98)
+	pid.output_limits = (0, 100)
+
     app.run(host='0.0.0.0')
