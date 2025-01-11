529d064bfeb886b2094fbaf11528c8d1f2cd8071
JCompagnat <37506595+JCompagnat@users.noreply.github.com>
2025-01-11 11:02:13 -0500

    WIP

--- __init__.py
+++ __init__.py
@@ -16,16 +16,16 @@ manager = multiprocessing.Manager()
 shardedData = manager.dict()
 
 def initialize_shared_data():
-    shardedData['pid_p'] = 0
-    shardedData['pid_i'] = 0
-    shardedData['pid_d'] = 0
-    shardedData['brewTime'] = 0
-    shardedData['waterTemp'] = 0
-    shardedData['setTemp'] = 97.5
+	shardedData['pid_p'] = 0
+	shardedData['pid_i'] = 0
+	shardedData['pid_d'] = 0
+	shardedData['brewTime'] = 0
+	shardedData['waterTemp'] = 0
+	shardedData['setTemp'] = 97.5
 
 @app.route('/')  # the default is GET only
 def index():
-    initialize_shared_data()
+	initialize_shared_data()
 	worker_1 = multiprocessing.Process(name='worker 1', target=pid)
 	worker_2 = multiprocessing.Process(name='worker 2', target=brew)
 	worker_1.start()
